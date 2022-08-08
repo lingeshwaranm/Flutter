deb0472649c752d891810ddc9faabf3fd6879383
lingeshwaranm <lingeshwaran@datamoo.ai>
2022-08-08 23:29:48 +0530

    basic styles

--- lib/question.dart
+++ lib/question.dart
@@ -7,7 +7,16 @@ class Question extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(80),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 30),
+        textAlign: TextAlign.center,
+      ),
+    );
+
     // return MaterialApp(
     //     home: Scaffold(
     //     appBar: AppBar(
